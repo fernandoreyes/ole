397c7010367dcf3f9e21c6de277f44fc05cacbbe
Fernando <fernandoa.reyes@gmail.com>
2010-10-03 00:38:11 -0300

    comentario inicial yahoo git status

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ole::Application.config.secret_token = '2dc9319bfc2c7a889808590e249adfa536bb6026c175b712e680da02e43f3e50d301c5cbc36e706e8e54a967ea2c19c2103519a19b5411a11f9916d012fa4d9c'
